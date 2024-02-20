86d1dccbe9849f4746b124a35a2cf8fbb7ace867
Jelassi Saif <68915309+laakri@users.noreply.github.com>
2024-02-20 15:27:52 +0100

    fix navbar

--- frontend/src/authentication/Signup.tsx
+++ frontend/src/authentication/Signup.tsx
@@ -1,34 +1,33 @@
-import React, { useState, useEffect } from 'react';
+import { useState } from "react";
 import { Flex, Input, Button, Text, Divider } from "@chakra-ui/react";
 import { NavLink } from "react-router-dom";
-import axios from 'axios';
+import axios from "axios";
 
 function Signup() {
     const [formData, setFormData] = useState({
-        name: '',
-        email: '',
-        password: ''
+        name: "",
+        email: "",
+        password: "",
     });
 
-    useEffect(() => {
-        console.log('Signup component mounted');
-    }, []); // Le tableau vide en tant que deuxième argument signifie que useEffect s'exécutera uniquement lors du montage initial
-
-    const handleChange = (e:any) => {
+    const handleChange = (e: any) => {
         setFormData({
             ...formData,
-            [e.target.name]: e.target.value
+            [e.target.name]: e.target.value,
         });
     };
 
-    const handleSubmit = async (e:any) => {
+    const handleSubmit = async (e: any) => {
         e.preventDefault();
 
         try {
-            const response = await axios.post('http://localhost:8000/api/register', formData);
-            console.log('Response:', response.data);
+            const response = await axios.post(
+                "http://localhost:8000/api/register",
+                formData
+            );
+            console.log("Response:", response.data);
         } catch (error) {
-            console.error('Error:', error);
+            console.error("Error:", error);
         }
     };
 
@@ -43,14 +42,16 @@ function Signup() {
             <Text fontSize="2xl">Create an Account</Text>
             <Divider my={8} w={300} />
 
-            <Flex flexDirection={"column"} mb={5}>
-                <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit}>
+                <Flex flexDirection={"column"} mb={5}>
                     <Input
                         variant="filled"
                         placeholder="Full Name"
                         mb={3}
                         w={300}
-                        name="name" value={formData.name} onChange={handleChange}
+                        name="name"
+                        value={formData.name}
+                        onChange={handleChange}
                     />
                     <Input
                         variant="filled"
@@ -58,7 +59,9 @@ function Signup() {
                         type="email"
                         mb={3}
                         w={300}
-                        name="email" value={formData.email} onChange={handleChange}
+                        name="email"
+                        value={formData.email}
+                        onChange={handleChange}
                     />
                     <Input
                         variant="filled"
@@ -66,13 +69,15 @@ function Signup() {
                         type="password"
                         mb={6}
                         w={300}
-                        name="password" value={formData.password} onChange={handleChange}
+                        name="password"
+                        value={formData.password}
+                        onChange={handleChange}
                     />
                     <Button type="submit" colorScheme="blue" mb={3}>
                         Sign Up
                     </Button>
-                </form>
-            </Flex>
+                </Flex>
+            </form>
 
             <Text mt={4} fontSize="sm">
                 Already have an account?
